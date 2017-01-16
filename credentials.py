009f0f660d8c8054544f0d97d08035d1f763e1a9
LtGlahn <jensen72@gmail.com>
2017-01-16 10:14:29 +0100

    Hiding credential info from public repos

---
+++ credentials.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Keeps credentials away from github and other public repos. 
+
+Created on Mon Jan 16 10:06:50 2017
+
+@author: jajens
+"""
+
+
+def credentials():
+"""Returns login info: { 'yoursystem' : { 'user' : 'foo', 'pw' : 'baa' }}""" 
+
+    cred = {  'yoursystem' : { 
+                              'user' : foo, 
+                              'pw' : baa
+                          }
+                }
+                
+    return cred 
+
