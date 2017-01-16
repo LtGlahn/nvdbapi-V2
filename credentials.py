542e72dbc5dbdc9114132379b27298738cf9b55f
LtGlahn <jensen72@gmail.com>
2017-01-16 11:40:06 +0100

    Hiding credentials in external file outside repos

--- credentials.py
+++ credentials.py
@@ -6,16 +6,30 @@ Created on Mon Jan 16 10:06:50 2017
 
 @author: jajens
 """
+import json
 
 
 def credentials():
-"""Returns login info: { 'yoursystem' : { 'user' : 'foo', 'pw' : 'baa' }}""" 
+    """Returns login info: { "yoursystem" : { "user" : "foo", "pw" : "baa" }}""" 
 
-    cred = {  'yoursystem' : { 
-                              'user' : foo, 
-                              'pw' : baa
+    mycredfile = '../credentials.json' 
+    cred = {  "yoursystem" : { 
+                              "user" : "foo", 
+                              "pw" : "baa"
                           }
                 }
+    try: 
+        with open(mycredfile) as cf: 
+            tmp = json.load( cf) 
+        
+        # python 2 merge dict operation 
+        # overwriting the data found here. 
+        # http://stackoverflow.com/questions/38987/how-to-merge-two-python-dictionaries-in-a-single-expression
+        z = cred.copy()
+        z.update(tmp)
+        return z 
+    except FileNotFoundError:
+        print( 'Could not load credentials file:' + mycredfile )
+        return cred
                 
-    return cred 
 
