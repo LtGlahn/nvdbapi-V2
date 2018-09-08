fdfa478854aa632d925771c2afcb15db8e861ad6
Jensen Jan Kristian <jan.kristian.jensen@vegvesen.no>
2018-09-08 08:24:31 +0200

    fiks av forrige

--- temp_underutvikling.py
+++ temp_underutvikling.py
@@ -139,7 +139,7 @@ def sjekkslekta( slektliste,  egengeomtype=['Geometri, Punkt',
                                     str(barn['type']['id']) + '/' + str(vegobj), 
                                     parametre=parametre) 
                         except ValueError as myerr: 
-                            print( myerr.message) 
+                            print( myerr) 
                         else:     
                             if r and sjekkellipsoidehoyde(r, egengeomtype): 
                                 resultat.append( r)
