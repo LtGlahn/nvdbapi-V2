7a61af2bf7c2d3dc6cddb782a2427a77f75aa492
LtGlahn <jensen72@gmail.com>
2018-10-22 14:21:55 +0200

    Opprydding, skiller ut work-in-progress

--- ulykkesrelasjoner-enheter-person.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Script som laster ned ulykkesinvolverte enheter og ulykkesinvolverte personer
-som har relasjon til de oppgitte trafikkulykke-id'ene. 
-"""
-
-# Vil bruke de nye funksjonene for egenskapsverdier egenskapverdier_fastskjema
-
